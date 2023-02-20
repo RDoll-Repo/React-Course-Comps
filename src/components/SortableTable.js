b16e4045faa932fd9b79737f4bd2cdcfcd99c1b3
Roland Doll <rdoll007@gmail.com>
2023-02-20 01:16:39 -0500

    icon prototype

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -26,7 +26,11 @@ function SortableTable(props) {
 
         return {
             ...column,
-            header: () => <th onClick={() => handleClick(column.label)}>{column.label}</th>,
+            header: () => 
+                <th onClick={() => handleClick(column.label)}>
+                    {getIcons(column.label, sortBy, sortOrder)}
+                    {column.label}
+                </th>
         };
     });
 
@@ -59,6 +63,20 @@ function SortableTable(props) {
 
     </div>
 
+}
+
+function getIcons(label, sortBy, sortOrder) {
+    if (label !== sortBy) {
+        return 'Show both icons'
+    }
+
+    if (sortOrder === null) {
+        return 'show both icons'
+    } else if (sortOrder === 'asc') {
+        return 'show up icon'
+    } else if (sortOrder === 'desc') {
+        return 'show down icon'
+    }
 }
 
 export default SortableTable;
